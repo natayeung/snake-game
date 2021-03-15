ac0681162be16fc67ab6e9be3489cccdde0604c8
Natalie Yeung <natayeung@protonmail.com>
2021-03-15 21:30:04 +0000

    - Switched to use logback as log4j2 is not modularised and jlink does not work with automatic modules. - There is a slight issue with logback (See https://jira.qos.ch/browse/LOGBACK-1515). Added "requires transitive java.naming" to module descriptor as a temporary workaround.

--- gui/src/main/java/module-info.java
+++ gui/src/main/java/module-info.java
@@ -3,6 +3,8 @@ module com.natay.games.snake.gui {
     requires javafx.controls;
     requires javafx.media;
     requires org.slf4j;
+    requires ch.qos.logback.classic;
+    requires transitive java.naming; // Added for a known issue with logback (https://jira.qos.ch/browse/LOGBACK-1515)
 
     opens com.natay.games.snake.gui to javafx.controls;
 
